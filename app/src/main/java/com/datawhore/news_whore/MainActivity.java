e9189807b7476900ab69b2236f6019a79cf015cb
c0d3rm0nk3y <code4chaosmobile@gmail.com>
2014-11-21 11:04:11 -0600

    added: get auth token from google services for authorizing in my REST api

--- app/src/main/java/com/datawhore/news_whore/MainActivity.java
+++ app/src/main/java/com/datawhore/news_whore/MainActivity.java
@@ -3,30 +3,37 @@ package com.datawhore.news_whore;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.android.gms.common.AccountPicker;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_main);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
   }
 
   static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
 
   private void pickUserAccount() {
-  String[] accountTypes = new String[]{"com.google"};
-  Intent intent = AccountPicker.newChooseAccountIntent(null, null,
-      accountTypes, false, null, null, null, null);
-  startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+    String[] accountTypes = new String[]{"com.google"};
+    Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+        accountTypes, false, null, null, null, null);
+    startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
   }
 
   String mEmail; // Received from newChooseAccountIntent(); passed to getToken()
@@ -50,29 +57,57 @@ public class MainActivity extends Activity {
   }
 
   private void getUsername() {
+    new RetrieveTokenTask().execute(mEmail);
+  }
 
+  private static final String TAG = "RetrieveAccessToken";
+  private static final int REQ_SIGN_IN_REQUIRED = 55664;
+
+  private class RetrieveTokenTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... params) {
+      String accountName = params[0];
+      String scopes = "oauth2:profile email";
+      String token = null;
+      try {
+        token = GoogleAuthUtil.getToken(getApplicationContext(), accountName, scopes);
+      } catch (IOException e) {
+        Log.e(TAG, e.getMessage());
+      } catch (UserRecoverableAuthException e) {
+        startActivityForResult(e.getIntent(), REQ_SIGN_IN_REQUIRED);
+      } catch (GoogleAuthException e) {
+        Log.e(TAG, e.getMessage());
+      }
+      return token;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+      super.onPostExecute(s);
+      ((TextView)findViewById(R.id.status)).setText(s);
+    }
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-  // Inflate the menu; this adds items to the action bar if it is present.
-  getMenuInflater().inflate(R.menu.menu_main, menu);
-  return true;
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-  // Handle action bar item clicks here. The action bar will
-  // automatically handle clicks on the Home/Up button, so long
-  // as you specify a parent activity in AndroidManifest.xml.
-  int id = item.getItemId();
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
 
-  //noinspection SimplifiableIfStatement
-  if (id == R.id.action_settings) {
-    pickUserAccount();
-    return true;
-  }
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      pickUserAccount();
+      return true;
+    }
 
-  return super.onOptionsItemSelected(item);
+    return super.onOptionsItemSelected(item);
   }
 }
